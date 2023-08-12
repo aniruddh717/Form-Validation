0c66ec03e33cce01b0be292fe25659e2566ade31
aniruddhWeb <aniruddh@websoham.com>
2023-08-13 02:26:01 +0530

    email validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,21 +3,29 @@ import { useState, useEffect } from "react"; //not usinf useRef anymore
 const SimpleInput = (props) => {
   // const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [formIsValid, setFormIsValid] = useState(false)
+  const [formIsValid, setFormIsValid] = useState(false);
   // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false); rather than using a state we can use a var and define the value there
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
-  
+
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+
   const enteredNameIsValid = enteredName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  const enteredEmailIsValid =
+  enteredEmail.trim() !== "" &&
+    enteredEmail.includes("@") &&
+    enteredEmail.includes(".com");
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+
   useEffect(() => {
-    if(enteredNameIsValid){
-      setFormIsValid(true)
-    } else{
-      setFormIsValid(false)
+    if (enteredNameIsValid && enteredEmailIsValid) {
+      setFormIsValid(true);
+    } else {
+      setFormIsValid(false);
     }
-    
-  }, [enteredNameIsValid])
+  }, [enteredNameIsValid, enteredEmailIsValid]);
 
   const addNameHandler = (event) => {
     setEnteredName(event.target.value);
@@ -27,25 +35,33 @@ const SimpleInput = (props) => {
     // }
   };
 
+  const addEmailHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-
     // if (enteredName.trim() === "") {     now there is no need of if satatement bcz we using a enteredNameIsValid var now
     //   setEnteredNameIsValid(false);
     // }
+  };
+
+  const nameEmailBlurHandler = (event) => {
+    setEnteredEmailTouched(true);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
+    setEnteredEmailTouched(true);
 
     // if (enteredName.trim() === "") {
     //   setEnteredNameIsValid(false);
     //   return;
     // }
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       // updated above condition while using enteredNameIsValid
       return;
     }
@@ -57,10 +73,16 @@ const SimpleInput = (props) => {
     // nameInputRef.current.value = "";
     setEnteredName("");
     setEnteredNameTouched(false);
+    
+    setEnteredEmail("");
+    setEnteredEmailTouched(false);
   };
 
-
   const nameInputClass = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailInputClass = emailInputIsInvalid
     ? "form-control invalid"
     : "form-control";
 
@@ -77,7 +99,20 @@ const SimpleInput = (props) => {
           onBlur={nameInputBlurHandler}
         />
       </div>
-      {nameInputIsInvalid && <p>Name must be entered</p>}
+      {emailInputIsInvalid && <p>Please enter a valid name</p>}
+
+      <div className={emailInputClass}>
+        <label htmlFor="email">Your E-mail</label>
+        <input
+          // ref={nameInputRef}
+          value={enteredEmail}
+          type="email"
+          id="email"
+          onChange={addEmailHandler}
+          onBlur={nameEmailBlurHandler}
+        />
+      </div>
+      {emailInputIsInvalid && <p>Please enter a valid email</p>}
       <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
       </div>
