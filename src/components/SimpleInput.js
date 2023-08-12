a46d14daf837b9a59b99e1ae2513624ac4d40cb8
aniruddhWeb <aniruddh@websoham.com>
2023-08-13 00:36:53 +0530

    reducing states

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,40 +1,51 @@
-import { useState, useRef } from "react";
+import { useState } from "react"; //not usinf useRef anymore
 
 const SimpleInput = (props) => {
-  const nameInputRef = useRef();
+  // const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false); rather than using a state we can use a var and define the value there
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNameIsValid = enteredName.trim() !== "";
 
   const addNameHandler = (event) => {
     setEnteredName(event.target.value);
+
+    // if (event.target.value.trim() !== "") {   now there is no need of if satatement bcz we using a enteredNameIsValid var now
+    //   setEnteredNameIsValid(true);
+    // }
   };
 
-  const nameInputBlurHandler = event => {
-    setEnteredNameTouched(true)
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
-      return;
-    }
-  }
+    // if (enteredName.trim() === "") {     now there is no need of if satatement bcz we using a enteredNameIsValid var now
+    //   setEnteredNameIsValid(false);
+    // }
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNameIsValid(false);
+    // if (enteredName.trim() === "") {
+    //   setEnteredNameIsValid(false);
+    //   return;
+    // }
+
+    if (!enteredNameIsValid) {
+      // updated above condition while using enteredNameIsValid
       return;
     }
 
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
+    // const enteredValue = nameInputRef.current.value;   no more need of ref now
+    // console.log(enteredValue);
 
-    nameInputRef.current.value = "";
+    // nameInputRef.current.value = "";
     setEnteredName("");
+    setEnteredNameTouched(false);
   };
 
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
@@ -48,7 +59,7 @@ const SimpleInput = (props) => {
       <div className={nameInputClass}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
+          // ref={nameInputRef}
           value={enteredName}
           type="text"
           id="name"
